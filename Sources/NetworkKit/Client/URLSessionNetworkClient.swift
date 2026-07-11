c907d8d57afb488ee9a042dc21b9e834335605c5
Tris <hbabnd@gmail.com>
2026-07-12 04:13:49 +0900

    feat: implement URLSession request execution

--- Sources/NetworkKit/Client/URLSessionNetworkClient.swift
+++ Sources/NetworkKit/Client/URLSessionNetworkClient.swift
@@ -8,10 +8,10 @@
 import Foundation
 
 public final class URLSessionNetworkClient: NetworkClient {
-
+    
     private let configuration: NetworkConfiguration
     private let session: URLSession
-
+    
     public init(
         configuration: NetworkConfiguration,
         session: URLSession = .shared
@@ -19,11 +19,45 @@ public final class URLSessionNetworkClient: NetworkClient {
         self.configuration = configuration
         self.session = session
     }
-
+    
     public func request<T: Decodable>(
         endpoint: any Endpoint,
         responseType: T.Type
     ) async throws -> T {
-        fatalError("Not implemented yet.")
+
+        let request = try makeRequest(from: endpoint)
+
+        let (data, response) = try await session.data(for: request)
+
+        fatalError("Next step")
+    }
+    
+    // MARK: - Private
+    private func makeRequest(
+        from endpoint: any Endpoint
+    ) throws -> URLRequest {
+
+        var components = URLComponents(
+            url: configuration.baseURL.appendingPathComponent(endpoint.path),
+            resolvingAgainstBaseURL: false
+        )
+
+        components?.queryItems = endpoint.queryItems
+
+        guard let url = components?.url else {
+            throw NetworkError.invalidURL
+        }
+
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = endpoint.method.rawValue
+        request.timeoutInterval = configuration.timeout
+        endpoint.headers.forEach {
+            request.setValue($0.value, forHTTPHeaderField: $0.key)
+        }
+
+        request.httpBody = endpoint.body
+
+        return request
     }
 }
