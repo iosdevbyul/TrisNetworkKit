b70536b178d1c1ec8d763815f96c796ac0652b6e
Tris <hbabnd@gmail.com>
2026-07-12 03:46:12 +0900

    feat(endpoint): add default Endpoint implementations

---
+++ Sources/NetworkKit/Extension/📄 Endpoint+Default.swift
@@ -0,0 +1,23 @@
+//
+//  📄 Endpoint+Default.swift
+//  NetworkKit
+//
+//  Created by COMATOKI on 2026-07-12.
+//
+
+import Foundation
+
+public extension Endpoint {
+    
+    var headers: [String: String] {
+        [:]
+    }
+    
+    var queryItems: [URLQueryItem] {
+        []
+    }
+    
+    var body: Data? {
+        nil
+    }
+}
