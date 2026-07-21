32587ec88d3e7c4dd8a2dac71311a4de0fabc443
Tris <hbabnd@gmail.com>
2026-07-21 17:35:09 +0900

    test: add server error request test

--- Tests/NetworkKitTests/URLSessionNetworkClientTests.swift
+++ Tests/NetworkKitTests/URLSessionNetworkClientTests.swift
@@ -60,4 +60,63 @@ final class URLSessionNetworkClientTests: XCTestCase {
             )
         )
     }
+    
+    func test_request_whenResponseIs404_throwsServerError() async {
+
+        MockURLProtocol.response = HTTPURLResponse(
+            url: URL(string: "https://example.com/users/1")!,
+            statusCode: 404,
+            httpVersion: nil,
+            headerFields: nil
+        )
+
+        MockURLProtocol.responseData = Data()
+        MockURLProtocol.error = nil
+
+        let configuration = URLSessionConfiguration.ephemeral
+
+        configuration.protocolClasses = [
+            MockURLProtocol.self
+        ]
+
+        let session = URLSession(
+            configuration: configuration
+        )
+
+        let networkConfiguration = NetworkConfiguration(
+            baseURL: URL(string: "https://example.com")!
+        )
+
+        let client = URLSessionNetworkClient(
+            configuration: networkConfiguration,
+            session: session
+        )
+
+        do {
+            _ = try await client.request(
+                endpoint: MockUserEndpoint.user,
+                responseType: MockUser.self
+            )
+
+            XCTFail("Expected serverError, but request succeeded.")
+
+        } catch let error as NetworkError {
+
+            switch error {
+            case .serverError(let statusCode):
+                XCTAssertEqual(statusCode, 404)
+
+            default:
+                XCTFail(
+                    "Expected serverError, but received \(error)"
+                )
+            }
+
+        } catch {
+            XCTFail(
+                "Expected NetworkError.serverError, but received \(error)"
+            )
+        }
+    }
+    
 }
