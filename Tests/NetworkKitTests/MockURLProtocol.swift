9b6189ed8fc38fb40920ff036eca33ca30a25ca9
Tris <hbabnd@gmail.com>
2026-07-21 17:27:03 +0900

    test: add mock url protocol

---
+++ Tests/NetworkKitTests/MockURLProtocol.swift
@@ -0,0 +1,60 @@
+//
+//  MockURLProtocol.swift
+//  NetworkKit
+//
+//  Created by COMATOKI on 2026-07-21.
+//
+
+import Foundation
+
+final class MockURLProtocol: URLProtocol {
+
+    nonisolated(unsafe) static var response: HTTPURLResponse?
+    nonisolated(unsafe) static var responseData: Data?
+    nonisolated(unsafe) static var error: Error?
+
+    override class func canInit(
+        with request: URLRequest
+    ) -> Bool {
+        true
+    }
+
+    override class func canonicalRequest(
+        for request: URLRequest
+    ) -> URLRequest {
+        request
+    }
+
+    override func startLoading() {
+
+        if let error = Self.error {
+            client?.urlProtocol(
+                self,
+                didFailWithError: error
+            )
+            return
+        }
+
+        guard let response = Self.response else {
+            return
+        }
+
+        client?.urlProtocol(
+            self,
+            didReceive: response,
+            cacheStoragePolicy: .notAllowed
+        )
+
+        if let data = Self.responseData {
+            client?.urlProtocol(
+                self,
+                didLoad: data
+            )
+        }
+
+        client?.urlProtocolDidFinishLoading(self)
+    }
+
+    override func stopLoading() {
+    }
+}
